c34966ab4da5367a2dee76fe8b1883d29b79b6c5
NickBres <70432147+NickBres@users.noreply.github.com>
2023-02-16 14:14:27 +0200

    using rectangles to control the objects

--- main.py
+++ main.py
@@ -16,10 +16,17 @@ sky_surface = pygame.image.load(
     'graphics/Sky.png').convert()  # convert it to better format for pygame, for better performance
 ground_surface = pygame.image.load('graphics/ground.png').convert()
 snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()  # convert it without background
-
+player_surface = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
 # snail
 snail_x_pos = screen_width
-snail_y_pos = sky_surface.get_height() - snail_surface.get_height()  # on the ground
+snail_y_pos = sky_surface.get_height()
+snail_rect = snail_surface.get_rect(midbottom=(snail_x_pos, snail_y_pos))
+
+# player
+player_x_pos = 100
+player_y_pos = sky_surface.get_height()  # on the ground
+player_rect = player_surface.get_rect(
+    midbottom=(player_x_pos, player_y_pos))  # now positioning rectangle from midbottom point
 
 while True:  # main game loop
     for event in pygame.event.get():
@@ -35,10 +42,13 @@ while True:  # main game loop
     screen.blit(text_surface,
                 (screen_width / 2 - text_surface.get_width() / 2,
                  screen_height / 2 - text_surface.get_height() / 2))  # in the middle of the screen
-    screen.blit(snail_surface, (snail_x_pos, snail_y_pos))
+    screen.blit(snail_surface, snail_rect)
+    screen.blit(player_surface, player_rect)
 
-    snail_x_pos -= 3
-    if snail_x_pos <= -snail_surface.get_width(): snail_x_pos = screen_width
+    player_rect.left += 1
+
+    snail_rect.left -= 3
+    if snail_rect.right <= 0: snail_rect.left = screen_width
 
     pygame.display.update()
     clock.tick(60)  # limit the loop to 60 times per sec
