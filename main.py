3337e9a2cd6d4750b47e4305971951cb55d134e4
NickBres <70432147+NickBres@users.noreply.github.com>
2023-02-17 10:50:31 +0200

    sounds added

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ class Player(pygame.sprite.Sprite):
         self.player_walk = [self.player_walk1, self.player_walk2]
         self.player_index = 0
         self.player_jump = pygame.image.load('graphics/player/jump.png').convert_alpha()
+        self.jump_sound = pygame.mixer.Sound('audio/jump.mp3')
+        self.jump_sound.set_volume(0.3)
 
         self.x = x
         self.y = y
@@ -21,6 +23,7 @@ class Player(pygame.sprite.Sprite):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_SPACE] and self.rect.bottom >= self.y:
             self.gravity = -20
+            self.jump_sound.play()
 
     def apply_gravity(self):
         self.gravity += 1
@@ -104,6 +107,9 @@ font = pygame.font.Font('font/Pixeltype.ttf', 50)
 text_color = (64, 64, 64)
 start_time = 0
 best = 0
+bg_music = pygame.mixer.Sound('audio/music.wav')
+bg_music.set_volume(0.1)
+bg_music.play(loops=-1)
 
 # background
 sky_surf = pygame.image.load('graphics/Sky.png').convert()
